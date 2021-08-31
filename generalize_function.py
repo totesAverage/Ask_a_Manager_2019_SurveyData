495ce8f6b9e9bc86b28cd61d38b38d627b5c3fd0
Imeel Saleem <87979781+totesAverage@users.noreply.github.com>
2021-09-01 00:48:47 +0500

    Add files via upload
    More details about this function can be checked out here: https://towardsdatascience.com/simplify-your-dataset-cleaning-with-pandas-75951b23568e

---
+++ generalize_function.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  1 00:24:16 2021
+
+@author: ASUS
+"""
+
+def generalize(ser, match_name, default=None, regex=False, case=False):
+    """ Search a series for text matches.
+    Based on code from https://www.metasnake.com/blog/pydata-assign.html
+
+    ser: pandas series to search
+    match_name: tuple containing text to search for and text to use for normalization
+    default: If no match, use this to provide a default value, otherwise use the original text
+    regex: Boolean to indicate if match_name contains a  regular expression
+    case: Case sensitive search
+
+    Returns a pandas series with the matched value
+
+    """
+    seen = None
+    for match, name in match_name:
+        mask = ser.str.contains(match, case=case, regex=regex)
+        if seen is None:
+            seen = mask
+        else:
+            seen |= mask
+        ser = ser.where(~mask, name)
+    if default:
+        ser = ser.where(seen, default)
+    else:
+        ser = ser.where(seen, ser.values)
+    return ser
